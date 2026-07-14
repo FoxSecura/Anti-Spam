24a95755910e0fbde1e77ab9ff34c3a82c911678
GhostPunishR <68479390+GhostPunishR@users.noreply.github.com>
2026-07-14 10:56:51 +0200

    feat: export anti-spam enforcement API

--- src/adapters/discordjs/index.ts
+++ src/adapters/discordjs/index.ts
@@ -1,3 +1,12 @@
 export { DiscordJsAntiSpam } from "./DiscordJsAntiSpam.js";
+export { enforceAntiSpamIncident } from "./enforcement.js";
 export { toMessageEvent } from "./toMessageEvent.js";
-export type { DiscordJsAntiSpamAdapter, DiscordJsAntiSpamOptions } from "./types.js";
+export type {
+  DiscordJsAntiSpamAdapter,
+  DiscordJsAntiSpamEnforcementAction,
+  DiscordJsAntiSpamEnforcementErrorContext,
+  DiscordJsAntiSpamEnforcementOptions,
+  DiscordJsAntiSpamEnforcementResult,
+  DiscordJsAntiSpamEnforcementStatus,
+  DiscordJsAntiSpamOptions,
+} from "./types.js";
